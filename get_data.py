658337049dd39898b0680ec83aa45ba1bf0f5fdd
BanJeez <olabanji.ajayi@gmail.com>
2021-06-01 12:41:15 +0100

    connect to binance Pass

--- get_data.py
+++ get_data.py
@@ -1,6 +1,20 @@
 import config
 
-from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+from binance.client import Client
+
+#from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 
 
 client = Client(config.API_KEY, config.API_SECRET)
+
+prices = client.get_all_tickers()
+
+#for price in prices:
+   # print(price)
+
+candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_15MINUTE)
+
+for candlestick in candles:
+    print(candlestick)
+
+print(len(candles))
